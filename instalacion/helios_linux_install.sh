128427c573a8f71987b5e5d56aaa3e47de52bc9f
Miguel Yermo <miguelyermo@gmail.com>
2024-10-04 10:22:43 +0200

    Automatizacion version de python

--- instalacion/helios_linux_install.sh
+++ instalacion/helios_linux_install.sh
@@ -10,6 +10,10 @@
 # Get script dir
 SCRIPT_DIR=$(dirname -- "$(readlink -f "$0")")
 
+PYTHON_DOT_VERSION=3.12
+
+# Remove dot from version
+PYTHON_VERSION=$(echo $PYTHON_DOT_VERSION | tr -d '.')
 
 # Clone repository
 cd $HOME
@@ -24,16 +28,14 @@ sed -i 's/HELIOS_DIR=.*/HELIOS_DIR=$HOME\/git\/helios\//' $HOME/git/helios/scrip
 # Install dependencies
 cd helios
 mkdir -p lib
-scripts/build/build_all_libs.sh  -p 3.12
+scripts/build/build_all_libs.sh -p $PYTHON_DOT_VERSION
 
 
 # Build HELIOS++
 cp -r "${SCRIPT_DIR}/helios_cmake/"* .
 mkdir cmake-build-release
 cd cmake-build-release
-cmake -DBUILD_TYPE=Release -DPYTHON_VERSION=312 -DPYTHON_BINDING=1 -DBOOST_DYNAMIC_LIBS=0 \
-    -DPCL_BINDING=0 -DBUDDING_METRICS=0 -DDATA_ANALYTICS=0 \
-    ..
+cmake -DBUILD_TYPE=Release -DPYTHON_VERSION=$PYTHON_VERSION -DPYTHON_BINDING=1 -DBOOST_DYNAMIC_LIBS=0 -DPCL_BINDING=0 -DBUDDING_METRICS=0 -DDATA_ANALYTICS=0 ..
 make -j 4
 
 # Copy HELIOS++ binary
