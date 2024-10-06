f62af3c55ab313b32f7579da0148733292376649
Miguel Yermo <miguelyermo@gmail.com>
2024-10-06 21:43:36 +0200

    Añadida exposision de puerto para jupyter notebooks

--- instalacion/build_exec_docker_image.sh
+++ instalacion/build_exec_docker_image.sh
@@ -7,4 +7,4 @@ IMG_NAME=helios/workshop:latest
 CONTAINER_NAME=helios_workshop
 
 docker build -t $IMG_NAME .
-docker run -v $SCRIPT_DIR/..:/root/vlsdl_workshop2024 -it --name $CONTAINER_NAME --memory=8g $IMG_NAME /bin/bash 
+docker run -v $SCRIPT_DIR/..:/root/vlsdl_workshop2024 -p 8888:8888 -it --name $CONTAINER_NAME $IMG_NAME /bin/bash 
